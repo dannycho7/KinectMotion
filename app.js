a9beb2f3b4888b9da11ccde5ea735020ebd2f349
dannycho7 <dannycho7@gmail.com>
2017-04-22 00:14:57 -0700

    Final configs

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ if(kinect.open()) {
     console.log("Kinect Opened");
     //listen for body frames
     kinect.on('bodyFrame', function(bodyFrame){
-        for(var i = 0;  i < bodyFrame.bodies.length; i++) {
-        	console.log(bodyFrame.bodies[i]);
-        }
+    	for(var i = 0; i < bodyFrame.bodies.length; i++){
+		if(bodyFrame.bodies[i].tracked){
+			//console.log(bodyFrame.bodies[i]);
+			var rightHandPos = bodyFrame.bodies[i].joints[Kinect2.JointType.handRight].cameraX;
+			var leftHandPos = bodyFrame.bodies[i].joints[Kinect2.JointType.handLeft].cameraX;
+			var headPos = bodyFrame.bodies[i].joints[Kinect2.JointType.head].cameraX;
+			if ( rightHandPos - 0.15 < headPos || leftHandPos - 0.15 < headPos) {
+				console.log("Hand touching head");
+			}
+			else {
+				console.log();
+			}
+		}
+	}
     });
 
     //request body frames
     kinect.openBodyReader();
 
-    //close the kinect after 5 seconds
+    //close the kinect after 200 seconds
     setTimeout(function(){
         kinect.close();
         console.log("Kinect Closed");
-    }, 60000);
+    }, 200000);
 }
